4a254b723252384a349a24cd08946e8760034411
tbdmb41 <32739958+tbdmb41@users.noreply.github.com>
2018-02-25 13:33:48 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The number ccould not be converted to a byte");
+               
+            }
+
+        }
+    }
+}
